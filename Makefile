121c0417aba6f3f796843e796ce456030c70a7d1
Saleh arafati <msaonfield@gmail.com>
2020-10-05 15:55:36 +0330

    fix bug lcm in file

--- Makefile
+++ Makefile
@@ -167,6 +167,7 @@ mkfs: mkfs.c fs.h
 
 UPROGS=\
 	_cat\
+	_lcm\
 	_echo\
 	_forktest\
 	_grep\
@@ -181,7 +182,6 @@ UPROGS=\
 	_usertests\
 	_wc\
 	_zombie\
-	_lcm\
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
@@ -249,7 +249,7 @@ qemu-nox-gdb: fs.img xv6.img .gdbinit
 # check in that version.
 
 EXTRA=\
-	lcm.c mkfs.c ulib.c user.h cat.c echo.c forktest.c grep.c kill.c\
+	mkfs.c ulib.c user.h cat.c echo.c forktest.c grep.c kill.c lcm.c\
 	ln.c ls.c mkdir.c rm.c stressfs.c usertests.c wc.c zombie.c\
 	printf.c umalloc.c\
 	README dot-bochsrc *.pl toc.* runoff runoff1 runoff.list\
--- lcm.c
+++ lcm.c
@@ -1,7 +1,12 @@
-
+#include "param.h"
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "fs.h"
+#include "fcntl.h"
+#include "syscall.h"
+#include "traps.h"
+#include "memlayout.h"
 int lcm_2(int a,int b){
     int res,temp;
     if(a > b){
@@ -22,23 +27,14 @@ int lcm_2(int a,int b){
 }
 int main(int argc, char **argv)
 {
+    char str[10] = "";
+    char endl = '\n';
     int fd,count;
-    fd = open("lcm_result.txt",0);
+    fd = open("lcm_result.txt",O_CREATE|O_RDWR);
     if (fd == -1) {
-        //consputc('c');
+        printf(1,"cant open or create file\n");
     }
-    for (int i = 1; i < argc; ++i)
-    {
-        count = write(fd, argv[i], sizeof(argv[i]));
-        //char s = '\n';
-        //count = write(fd, &s,  1);
-        //printf("salam\n");
-        if (count == -1) {
-            //consputc('c');
-        }
-        //printf("%s\n",argv[i]);
-    }
-    //printf("saleh\n");
+    //printf("saleh\n");    
     int javab = 1;
     int temp = 1;
     for (int i = 1; i < argc; ++i)
@@ -49,6 +45,24 @@ int main(int argc, char **argv)
         //printf("msa2\n");
     }
     printf(1,"%d\n",javab);
+    int j = 0;
+    while(javab > 0){
+        str[j] = (javab % 10) + '0';
+        javab = javab / 10;
+        j++;
+    }
+    //printf(1,"%s\n",str);
+    while(j >= 0){
+        j --;
+        count = write(fd, &str[j], 1);
+    }
+    count = write(fd, &endl, 1);
+    //char s = '\n';
+    //count = write(fd, &s,  1);
+    //printf("salam\n");
+    if (count == -1) {
+            printf(1,"cant write in to file\n");
+    }
     //consputc('c');
     close(fd);
     exit();
